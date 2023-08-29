9d0299508885364f75f643ecb21bd20b6dec1859
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-29 19:56:49 +0530

    sync functionality added

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -1,10 +1,12 @@
 const fs = require('fs/promises');
+const fsSync = require('fs');
 const path = require('path');
 
 (async () => {
   console.time('writeMany');
-  for (let i = 0; i < 10000; i++) {
-    await fs.appendFile(path.join(__dirname, 'test.txt'), 'test\n');
+  for (let i = 0; i < 10 * 1000; i++) {
+    // await fs.appendFile(path.join(__dirname, 'test.txt'), 'test\n');
+    fsSync.appendFileSync(path.join(__dirname, 'test.txt'), 'test\n');
   }
   console.timeEnd('writeMany');
 })();

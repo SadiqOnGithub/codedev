faa51bb6453cb3482600779e63bcb7328817e717
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-29 23:35:29 +0530

    callback functionality added; Benchmark added - RAM, Execution time

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -4,9 +4,18 @@ const path = require('path');
 
 (async () => {
   console.time('writeMany');
-  for (let i = 0; i < 10 * 1000; i++) {
-    // await fs.appendFile(path.join(__dirname, 'test.txt'), 'test\n');
-    fsSync.appendFileSync(path.join(__dirname, 'test.txt'), 'test\n');
+  for (let i = 0; i < 8000; i++) {
+    // Execution time 6s;
+    // RAM 15-20
+    // await fs.appendFile(path.join(__dirname, 'test.txt'), `${i}\n`);
+
+    // Execution time 3s;
+    // RAM <= 15
+    // fsSync.appendFileSync(path.join(__dirname, 'test.txt'), `${i}\n`);
+
+    // Execution time 500ms;
+    // RAM < 30
+    //  fsSync.appendFile(path.join(__dirname, 'test.txt'), `${i}\n`, (err) => err && console.log(err));
   }
   console.timeEnd('writeMany');
 })();

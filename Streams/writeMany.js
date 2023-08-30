8fac4f99ac436dc1d4fb2bb7b0dd64910f2fb41c
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-30 23:44:40 +0530

    Streams/ file organised & new bench mark added for 500*1000

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -1,35 +1,65 @@
+// const fs = require('fs/promises');
+// const fsSync = require('fs');
+// const path = require('path');
 
-// const fs = require("node:fs/promises");
 
-// Execution time 500ms - 1.2s;
-// RAM 15
-// (async () => {
-//   console.time("writeMany");
-//   const fileHandle = await fs.open("test.txt", "w");
+(async () => {
+  console.time('writeMany');
+  const filePath = path.join(__dirname, 'test.txt');
 
-//   for (let i = 0; i < 8000; i++) {
-//     await fileHandle.write(`${i}\n`);
-//   }
-//   console.timeEnd("writeMany");
-// })();
+  for (let i = 0; i < 8000; i++) {
+    // for 8000 ittiration
+
+    // Execution time 6s;
+    // RAM 15-20
+    await fs.appendFile(filePath, `${i}\n`);
+
+    // Execution time 3s;
+    // RAM <= 15
+    fsSync.appendFileSync(filePath, `${i}\n`);
+
+    // Execution time 500ms;
+    // RAM < 30
+    fsSync.appendFile(filePath, `${i}\n`, (err) => err && console.log(err));
+
+  }
+  console.timeEnd('writeMany');
+});
+
+
+const fs = require("node:fs/promises");
+
+(async () => {
+  console.time("writeMany");
+  const fileHandle = await fs.open("test.txt", "w");
+
+  for (let i = 0; i < 500 * 1000; i++) {
+    // for 500*1000 ittiration
+    // Execution time 30-50s;
+    // RAM 50mb
+    await fileHandle.write(`${i}\n`);
+  }
+  console.timeEnd("writeMany");
+})();
 
 
 
 // const fs = require("node:fs");
 
-// Execution time 150ms - 250ms;
-// RAM < 1
-// (async () => {
-//   console.time("writeMany");
-//   fs.open("test.txt", "w", (err, fd) => {
-//     for (let i = 0; i < 8000; i++) {
-//       const buff = Buffer.from(`${i}\n`, "utf-8");
-//       fs.writeSync(fd, buff);
-//     }
+(async () => {
+  console.time("writeMany");
+  fs.open("test.txt", "w", (err, fd) => {
+    for (let i = 0; i < 500 * 1000; i++) {
+      // for 500*1000 ittiration
+      // Execution time 6.5s;
+      // RAM 19-22
+      fs.writeSync(fd, `${i}\n`);
+    }
 
-//     console.timeEnd("writeMany");
-//   });
-// })();
+    console.timeEnd("writeMany");
+  });
+});
+
 
 
 // const fs = require('fs/promises');
@@ -37,31 +67,17 @@
 // const path = require('path');
 
 
-// (async () => {
-//   console.time('writeMany');
-//   const fileHandle = await fs.open(filePath, 'w');
-//   const filePath = path.join(__dirname, 'test.txt');
-//   const stream = fileHandle.createWriteStream();
+(async () => {
+  console.time('writeMany');
+  const filePath = path.join(__dirname, 'test.txt');
+  const fileHandle = await fs.open(filePath, 'w');
+  const stream = fileHandle.createWriteStream();
 
-//   for (let i = 0; i < 8000; i++) {
-    // for 8000 ittiration
-    
-    // Execution time 6s;
-    // RAM 15-20
-    // await fs.appendFile(filePath, `${i}\n`);
-
-    // Execution time 3s;
-    // RAM <= 15
-    // fsSync.appendFileSync(filePath, `${i}\n`);
-
-    // Execution time 500ms;
-    // RAM < 30
-    //  fsSync.appendFile(filePath, `${i}\n`, (err) => err && console.log(err));
-
-    // Execution time < 30ms;
-    // RAM < 1mb
-//     stream.write(`${i}\n`);
-
-//   }
-//   console.timeEnd('writeMany');
-// });
+  for (let i = 0; i < 500 * 1000; i++) {
+    // for 500*1000 ittiration
+    // Execution time 350ms-450ms;
+    // RAM 130mb
+    stream.write(`${i}\n`);
+  }
+  console.timeEnd('writeMany');
+});

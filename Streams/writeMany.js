cfcc8bd72ce568f738d84c50e3e03e90de4600b2
sadiqonWSL <sadiqonemail@gmail.com>
2023-09-04 00:57:39 +0530

    writing on file without increasing your RAM. This is high level nerdy stuff

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -62,9 +62,9 @@
 
 
 
-const fs = require('fs/promises');
-const fsSync = require('fs');
-const path = require('path');
+// const fs = require('fs/promises');
+// const fsSync = require('fs');
+// const path = require('path');
 
 
 (async () => {
@@ -103,4 +103,55 @@ const path = require('path');
   console.log(stream.writableLength);
   // fileHandle.close();
   console.timeEnd('writeMany');
+});
+
+
+
+
+const fs = require("node:fs/promises");
+
+(async () => {
+  console.time("writeMany");
+  const fileHandle = await fs.open("test.txt", "w");
+
+  const stream = fileHandle.createWriteStream();
+
+  console.log(stream.writableHighWaterMark);
+
+  // 8 bits = 1 byte
+  // 1000 bytes = 1 kilobyte
+  // 1000 kilobytes = 1 megabyte
+
+  let i = 0;
+
+  const numberOfWrites = 10000000;
+
+  const writeMany = () => {
+    while (i < numberOfWrites) {
+      const buff = Buffer.from(` ${i} `, "utf-8");
+
+      // this is our last write
+      if (i === numberOfWrites - 1) {
+        return stream.end(buff);
+      }
+
+      // if stream.write returns false, stop the loop
+      if (!stream.write(buff)) break;
+
+      i++;
+    }
+  };
+
+  writeMany();
+
+  // resume our loop once our stream's internal buffer is emptied
+  stream.on("drain", () => {
+    // console.log("Drained!!!");
+    writeMany();
+  });
+
+  stream.on("finish", () => {
+    console.timeEnd("writeMany");
+    fileHandle.close();
+  });
 })();
